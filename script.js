1441201aa811d334e97360852d564d1ccf97297e
Wilson <jrextreme3@gmail.com>
2025-03-17 05:47:41 -0300

    Primeiro commit - Adicionando código

---
+++ script.js
@@ -0,0 +1,15 @@
+function botaoA(){
+    alert("Errado, pense melhor");
+}
+function botaoB(){
+    alert("Errado, pense melhor");
+}
+function botaoC(){
+    alert("Errado, pense melhor");
+}
+function botaoD(){
+    alert("Correto, parabens pelo bom gosto");
+}
+function git(){
+    window.location.href="https://github.com/WilsonJr001";
+}
